2ac74cc9e9c933d1c4c3b906b261c9f71c09c809
brrcrites <brrcrites@gmail.com>
2020-04-05 22:37:10 -0700

    Update server to actually send text messages
    * Add parsing of the request body as a json object
    
    * Add twilio sms number, sid, and auth token as required by the
    .env file and throw errors if they are missing
    
    * Updates /send-sms endpoint to actually send an sms based on body

--- server/app/server.js
+++ server/app/server.js
@@ -1,12 +1,41 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 import cors from 'cors';
+import twilio from 'twilio';
+
+// Make sure we have the .env values we need before booting the server
+if(!process.env.TWILIO_SMS_NUMBER) {
+    throw('Missing TWILIO_SMS_NUMBER in .env file');
+}
+if(!process.env.TWILIO_ACCOUNT_SID) {
+    throw('Missing TWILIO_ACCOUNT_SID in .env file');
+}
+if(!process.env.TWILIO_AUTH_TOKEN) {
+    throw('Missing TWILIO_AUTH_TOKEN in .env file');
+}
 
 const app = express();
-
 app.use(cors());
+app.use(bodyParser.json());
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 app.post('/send-sms', function(req, res, next) {
-    res.send('POST request to /send-sms')
+    console.log(`body: ${JSON.stringify(req.body)}`);
+    client.messages.create({
+        body: req.body.message,
+        from: process.env.TWILIO_SMS_NUMBER,
+        to: req.body.toNumber
+    })
+    .then(
+        () => { res.send('SUCCESS - POST request to /send-sms'); }
+    )
+    .catch(
+        (error) => {
+            console.error(error);
+            res.send('ERROR - POST request to /send-sms');
+        }
+    )
 });
 
 const PORT = process.env.PORT || 8080
